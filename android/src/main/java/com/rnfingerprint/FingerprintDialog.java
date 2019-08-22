fa994ee8dc2cd982e4cea6801d89a4c926d6953b
huawei zhang <zhanghw@yingmi.cn>
2019-08-22 11:42:08 +0800

    fix dialog auth crash problem

--- android/src/main/java/com/rnfingerprint/FingerprintDialog.java
+++ android/src/main/java/com/rnfingerprint/FingerprintDialog.java
@@ -106,6 +106,7 @@ public class FingerprintDialog extends DialogFragment implements FingerprintHand
     @Override
     public void onPause() {
         super.onPause();
+        dismiss();
         if (this.isAuthInProgress) {
             this.mFingerprintHandler.endAuth();
             this.isAuthInProgress = false;
@@ -166,7 +167,9 @@ public class FingerprintDialog extends DialogFragment implements FingerprintHand
     @Override
     public void onAuthenticated() {
         this.isAuthInProgress = false;
-        this.dialogCallback.onAuthenticated();
+        if (this.dialogCallback != null) {
+            this.dialogCallback.onAuthenticated();
+        }
         dismiss();
     }
 
@@ -181,7 +184,9 @@ public class FingerprintDialog extends DialogFragment implements FingerprintHand
     public void onCancelled() {
         this.isAuthInProgress = false;
         this.mFingerprintHandler.endAuth();
-        this.dialogCallback.onCancelled();
+        if (this.dialogCallback != null) {
+            this.dialogCallback.onCancelled();
+        }
         dismiss();
     }
 }
